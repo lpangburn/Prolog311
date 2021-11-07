d586a10d4950048d9f70207d4e60f290a8f6095d
Liana <lianapangburn@gmail.com>
2021-11-07 16:47:17 -0500

    Liana code

--- prologExamples.pl
+++ prologExamples.pl
@@ -1,1 +1,23 @@
-hi
+%following code written by Liana Pangburn for Fall 2021 311-Principles of Programming Languages
+top('white blouse').
+top('black turtleneck').
+top('red t-shirt').
+
+bottom('jeans').
+bottom('chinos').
+bottom('shorts').
+
+shoes('boots').
+shoes('vans').
+shoes('sandals').
+
+accessory('oversized sunglasses').
+accessory('hat').
+accessory('scarf').
+accessory('backpack').
+
+make_Outfit(Top,Bottom,Shoe,Accessory) :-
+    top(Top),
+    bottom(Bottom),
+    shoes(Shoe),
+    accessory(Accessory).
