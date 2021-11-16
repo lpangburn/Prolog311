dbcd4240dbc82ffe136c28f04effbd6d9a9da211
Adam Khoukhi <77121453+CookieCoder17@users.noreply.github.com>
2021-11-15 23:33:31 -0500

    Update fibonacci.pl

--- fibonacci.pl
+++ fibonacci.pl
@@ -1,1 +1,17 @@
+launch_fib :-
+    write('Please input an integer: '),
+    read(N),nl, %read in an integer
+    write('The Fibonacci series of '),
+    write(N), write(' is: '),
+    write(0),write(,),write(1),
+    fib(N,0,1),nl.
 
+fib(N,X,Y) :-
+    (   
+    	N<2,!; % cut if it reaches a base case
+        SUM is X + Y,
+        write(,), write(SUM),
+        N1 is N-1,
+        fib(N1,Y,SUM) %recursive
+    ).
+   
